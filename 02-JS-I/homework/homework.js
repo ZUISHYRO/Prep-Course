253aef7412b287c75d0eb329c694443e13efed91
Zuishyro <kirito8900@hotmail.com>
2022-09-17 02:46:40 -0700

    test 4.0.1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -219,21 +219,13 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero > 0){
+  if (numero > 0) return 'es positivo';
 
-    return 'es positivo';
+  if(numero < 0) return 'es negativo';
 
-  }else if(numero < 0) {
+  if (numero === 0 ) return false;
 
-    return 'es negativo';
-
-  }else if (numero === 0 ){
-
-return false;
-
-  }
-
-
+  
 }
 
 function agregarSimboloExclamacion(str) {
