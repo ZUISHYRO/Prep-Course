ad19bbbf68bc19fe2350fa9cf240c874d4906c95
Zuishyro <kirito8900@hotmail.com>
2022-09-16 22:56:36 -0700

    test

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 1.05;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -29,8 +29,7 @@ function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
 
-  console.log(str);
-  return ;
+  return str
 
 }
 
