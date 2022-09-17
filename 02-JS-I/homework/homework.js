d56c8cd6bf79b9220e9b2ae57db25c4648b3651a
Zuishyro <kirito8900@hotmail.com>
2022-09-17 02:52:35 -0700

    test 4.0.5

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -221,11 +221,11 @@ function esPositivo(numero) {
   //Si el número es 0, devuelve false
   if (numero > 0) {
 
-    return 'es positivo';
+    return "es positivo";
 
   }else if(numero < 0){
 
-    return 'es negativo';
+    return "es negativo";
 
   }
   return false;
