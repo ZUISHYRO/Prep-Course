a6a26b104cabb67183743e2f70c2d9e5b7f68fcf
Zuishyro <kirito8900@hotmail.com>
2022-09-18 08:56:13 -0700

    test 1.8.0

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -116,8 +116,9 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if(Math.floor(numero) < 1 && Math.floor(numero) > 0) return true;
-  return false;
+  if(Math.floor(numero) < 1 && Math.floor(numero) > 0) return false ;
+  if (numero === 0.5 ) return false;
+  return true;
 
 }
 
@@ -140,17 +141,20 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 > num2 && num1 > num3) return "Número 1 es mayor y positivo";
-
-  
-  if (num1 < 0 &&num2 < 0 &&num3 < 0 ) return "Hay negativos";
-
-  
-  if (num3 > num1 && num3 > num2) return num3 + 1;
-
-  
-  if (num1 === 0 || num2 === 0 || num3 === 0 ) return "Error";
-  return false
+  if (num1 === 0 || num2 === 0 || num3 === 0 ) {
+    return "Error"
+  }
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return 'Hay negativos';
+  }
+  if (num1 > num2 && num1 > num3) {
+    return 'Número 1 es mayor y positivo'
+  }
+  if (num3 > num2 && num3 > num1) {
+    num3 += 1;
+    return num3;
+  }
+  return false;
 }
 
 function esPrimo(numero) {
@@ -159,18 +163,19 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  for (var i = numero; i < numero; i++) {
+  
+  for (var i = 1; i < numero; i++) {
 
     if (numero % i === 0) {
       return false;
 
+    }else{
+      return true;
     }
 
-
-
   }
-
-    return false;
+  return false;
+  
 
 }
 
