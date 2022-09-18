1b980e221df1283f78499454dd3027896bfa22bc
Zuishyro <kirito8900@hotmail.com>
2022-09-17 23:07:30 -0700

    test 1.1.3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -95,6 +95,11 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+if (numero === 10 || number === 5){
+  return "true";
+}else{
+  return "false"
+}
 }
 
 function estaEnRango(numero) {
