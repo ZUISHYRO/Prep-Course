efe97558ba48cb4e9d739bd2445be4cdbba85b54
Zuishyro <kirito8900@hotmail.com>
2022-09-18 07:35:28 -0700

    test 1.7.7

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -140,9 +140,9 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
   if (num1 > num2 && num1 > num3) return "Número 1 es mayor y positivo";
-  if (num1 < 0 ||num2 < 0 ||num3 < 0 ) return "Hay negativos";
+  if (num1 < 0 &&num2 < 0 &&num3 < 0 ) return "Hay negativos";
   if (num3 > num1 && num3 > num2) return num3 + 1;
-  if (num1 === 0 &&num2 === 0 &&num3 === 0 ) return "Error";
+  if (num1 === 0 || num2 === 0 || num3 === 0 ) return "Error";
   return false
 }
 
@@ -156,13 +156,15 @@ function esPrimo(numero) {
 
     if (numero % i === 0) {
       return false;
+    }else {
+      return true;
     }
 
-    return true;
+
 
   }
 
-  return true;
+    return true;
 
 }
 
