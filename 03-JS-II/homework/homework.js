71c0bcb7eee1c62bbfcc3513ed3a5fded4f34f6b
Zuishyro <kirito8900@hotmail.com>
2022-09-18 02:03:26 -0700

    test 1.7.4

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -155,12 +155,12 @@ function esPrimo(numero) {
   for (var i = 2; i < numero; i++) {
 
     if (numero % i === 0) {
-      return true;
+      return false;
     }
 
   }
 
-  return false;
+  return true;
 
 }
 
